4ee6fed3df1a6c26dc8a7798e7802a0472eee74a
daredevil-grbll <crazyzacke@gmail.com>
2022-11-11 02:19:33 -0800

    Creating unreal project

---
+++ Game/ShoppingSimulator/Source/ShoppingSimulator/ShoppingSimulator.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ShoppingSimulator.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShoppingSimulator, "ShoppingSimulator" );
