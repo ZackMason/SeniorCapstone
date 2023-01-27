c463fddd1b1f1113a9324a9c2bdc5c13a6f644a2
Zackery Mason-Blaug <crazyzacke@gmail.com>
2023-01-26 16:59:49 -0800

    Quick clarifying comment

--- Game/ShoppingSim/Assets/GameObjects/ShoppingCart/AIController/IAIController.cs
+++ Game/ShoppingSim/Assets/GameObjects/ShoppingCart/AIController/IAIController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public interface IAIController
 {
     public Vector2 GetMovement();
+    // note(zack): 3d world position that the arm should move to
     public Vector3 GetArmTarget();
         
     // note(zack): only used by player controller
