d239dbb1426d80131b5b623acc90c5e6c55e2656
ZackMason <crazyzacke@gmail.com>
2023-02-18 22:01:02 -0800

    Delete Game/ShoppingSimulator directory

--- Game/ShoppingSimulator/Source/ShoppingSimulator/ShoppingSimulator.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ShoppingSimulator.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShoppingSimulator, "ShoppingSimulator" );
