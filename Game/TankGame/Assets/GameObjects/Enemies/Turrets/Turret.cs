9cc9eaa3bc83b18afc580ee4bb6aff43d35c8760
HaroonKhan21 <72046470+HaroonKhan21@users.noreply.github.com>
2023-03-04 20:00:31 -0800

    Prefab set up
    Prefab set up but lacks targeting. Firing will be completed soon.

--- Game/TankGame/Assets/GameObjects/Enemies/Turrets/Turret.cs
+++ Game/TankGame/Assets/GameObjects/Enemies/Turrets/Turret.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public ITankBrain  _brain;
+    private IWeapon     _weapon;
+    public GameObject   TurretBody;
+    public GameObject   TurretBarrel;
+
+    void Awake()
     {
-        
+        Debug.Assert(TurretBody != null);
+        Debug.Assert(TurretBarrel != null);
+
+        if (_brain == null) {
+            _brain = GetComponent<ITankBrain>();
+        }
+        _weapon = GetComponentInChildren<IWeapon>();
+
+        Debug.Assert(_brain != null);
+        Debug.Assert(_weapon != null);
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector2 TurretInput = _brain.GetTurretInput() * Time.fixedDeltaTime * 100.0f;
+
+        if (Cursor.lockState == CursorLockMode.Locked) {
+            TurretBarrel.transform.Rotate(TurretInput.y, 0.0f, 0.0f, Space.Self);
+            TurretBody.transform.Rotate(0.0f, TurretInput.x, 0.0f, Space.Self);
+        }
+
+        if (_brain.WantToFire() && _weapon != null) {
+            _weapon.Fire();
+        }
     }
 }
