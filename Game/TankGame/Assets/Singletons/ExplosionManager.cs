55c26e358a6089ccdfc3552196e70d3534f26a99
ermoshkt <ermoshkt@oregonstate.edu>
2023-04-15 18:55:39 -0700

    hover sound added, spacial sound manager

--- Game/TankGame/Assets/Singletons/ExplosionManager.cs
+++ Game/TankGame/Assets/Singletons/ExplosionManager.cs
@@ -21,7 +21,7 @@ public class ExplosionManager : MonoBehaviour
 
     public void SpawnExplosion(Vector3 position, float radius, float power, float damage) {
         ParticleManager.Instance.TrySpawn(ExplosionParticles, position);
-        SoundManager.Instance.PlaySound(SoundAsset.Explosion);
+        SoundManager.Instance.PlaySound(SoundAsset.Explosion, position);
         
         var colliders = Physics.OverlapSphere(position, radius);
 
